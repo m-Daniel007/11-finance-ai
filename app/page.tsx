7a1f2bdcb15b5acb4f43efc4a4bdbc73787fe84d
Marcos <marcosdanieldossantos@yahoo.com.br>
2024-11-07 11:06:00 -0300

    chore: add git hook setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { dark, neobrutalism } from "@clerk/themes";
 import { redirect } from "next/navigation";
 
 const Home = async () => {
@@ -10,10 +9,7 @@ const Home = async () => {
   }
   return (
     <div className="flex h-full items-center justify-center">
-      <UserButton
-        showName
-        
-      />
+      <UserButton showName />
     </div>
   );
 };
